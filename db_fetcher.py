f4a8e11a83380fc3bc9c05d30dd657378f7a4c90
nikhilkumarsingh <nikhilksingh97@gmail.com>
2017-02-22 21:24:48 +0530

    specified path

--- db_fetcher.py
+++ db_fetcher.py
@@ -2,6 +2,9 @@ import os, random
 import sqlite3
 from datetime import datetime, timedelta
 
+path = '/home/swiftars/swift/'
+
+
 num_cities = 5
 num_companies = 2
 num_routes = num_cities*(num_cities - 1)
@@ -62,13 +65,13 @@ def fill_flights():
 
 def init_db():
     # create db from pre-defined schema
-    with open('./schema.sql', 'r') as f:
+    with open(path + 'schema.sql', 'r') as f:
         c.executescript(f.read())
     conn.commit()
 
 
 def register_user(name, email, pw):
-    conn = sqlite3.connect('./airlines.db')
+    conn = sqlite3.connect(path + 'airlines.db')
     c = conn.cursor()
 
     # insert user in passenger table
@@ -78,7 +81,7 @@ def register_user(name, email, pw):
 
 
 def update_flights():
-    conn = sqlite3.connect('./airlines.db')
+    conn = sqlite3.connect(path + 'airlines.db')
     c = conn.cursor()
 
     # fetch all current flights
@@ -125,7 +128,7 @@ def update_flights():
 
 
 def fetch_valid_users():
-    conn = sqlite3.connect('./airlines.db')
+    conn = sqlite3.connect(path + 'airlines.db')
     c = conn.cursor()
 
     c.execute("select email, pw_hash, name from passenger")
@@ -137,7 +140,7 @@ def fetch_valid_users():
 
 
 def fetch_locations():
-    conn = sqlite3.connect('./airlines.db')
+    conn = sqlite3.connect(path + 'airlines.db')
     c = conn.cursor()
 
     c.execute("select l_id, name from location")
@@ -147,7 +150,7 @@ def fetch_locations():
 
 
 def fetch_companies():
-    conn = sqlite3.connect('./airlines.db')
+    conn = sqlite3.connect(path + 'airlines.db')
     c = conn.cursor()
 
     c.execute("select c_id, name from company")
@@ -157,7 +160,7 @@ def fetch_companies():
 
 
 def fetch_routes_data(route_id = None):
-    conn = sqlite3.connect('./airlines.db')
+    conn = sqlite3.connect(path + 'airlines.db')
     c = conn.cursor()
 
     c.execute("select * from location")
@@ -179,7 +182,7 @@ def fetch_routes_data(route_id = None):
 
 
 def fetch_companies_data(company_id = None):
-    conn = sqlite3.connect('./airlines.db')
+    conn = sqlite3.connect(path + 'airlines.db')
     c = conn.cursor()
 
     if company_id == None:
@@ -196,7 +199,7 @@ def fetch_companies_data(company_id = None):
 
 
 def fetch_flights(from_id, to_id, dept_date, company_id, flight_id = None):
-    conn = sqlite3.connect('./airlines.db')
+    conn = sqlite3.connect(path + 'airlines.db')
     c = conn.cursor()
 
     if flight_id != None:
@@ -289,7 +292,7 @@ def fetch_flights(from_id, to_id, dept_date, company_id, flight_id = None):
 
 
 def commit_booking(f_id, email, btime, flight_type, price):
-    conn = sqlite3.connect('./airlines.db')
+    conn = sqlite3.connect(path + 'airlines.db')
     c = conn.cursor()
 
     if flight_type == 'eco':
@@ -338,7 +341,7 @@ def booking_template(bookings, cancellation):
 
 
 def fetch_bookings(email):
-    conn = sqlite3.connect('./airlines.db')
+    conn = sqlite3.connect(path + 'airlines.db')
     c = conn.cursor()
 
     c.execute("select p_id from passenger where email = ?", (email,))
@@ -369,7 +372,7 @@ def fetch_bookings(email):
 
 
 def cancel_booking(email, b_id):
-    conn = sqlite3.connect('./airlines.db')
+    conn = sqlite3.connect(path + 'airlines.db')
     c = conn.cursor()
 
     c.execute("select p_id from passenger where email = ?", (email,))
@@ -404,15 +407,15 @@ def cancel_booking(email, b_id):
 
 
 
-if not os.path.isfile('./airlines.db'):
-    conn = sqlite3.connect('./airlines.db')
+if not os.path.isfile(path + 'airlines.db'):
+    conn = sqlite3.connect(path + 'airlines.db')
     c = conn.cursor()
     init_db()
     fill_routes()
     fill_flights()
     update_flights()
 else:
-    conn = sqlite3.connect('./airlines.db')
+    conn = sqlite3.connect(path + 'airlines.db')
     c = conn.cursor()
 
 
